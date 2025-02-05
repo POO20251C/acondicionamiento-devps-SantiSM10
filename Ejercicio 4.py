6cf6fe06fe02fe74dad53c3abe7bdba499d18815
Santiago <sierramarinsantiago30@gmail.com>
2025-02-05 09:09:48 -0500

    eliminar ejercicio 4 del repositorio y del sistema

--- Ejercicio 4.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio 4: Invertir una Lista de Números
-
-def invertir_lista(numeros):
-    numeros_lista = list(map(int, numeros.split()))
-    numeros_lista.reverse()
-    return " ".join(map(str, numeros_lista))
-
-entrada = input()
-print(invertir_lista(entrada))
